377998468b6697e07b5e3cf39d0cb92707064f94
tokatu4561 <ryobsk4561@gmail.com>
2022-03-12 20:38:28 -0800

    ---

--- pages/posts/[...id].js
+++ pages/posts/[...id].js
@@ -13,7 +13,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+  const postData = await getPostData(params.id.join("/"));
   return {
     props: {
       postData,
