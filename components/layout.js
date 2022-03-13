a7914736b4c10f5c7669baaf9579bad7b0ee102b
tokatu4561 <ryobsk4561@gmail.com>
2022-03-12 18:33:19 -0800

    ssg getStaticProps()

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 const name = "Your Name";
 export const siteTitle = "Next.js Sample Website";
 
-export function Layout({ children, home }) {
+export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
