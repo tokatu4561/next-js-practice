cb8838b142265378081c6a53feb0ae014242e3b2
tokatu4561 <ryobsk4561@gmail.com>
2022-03-13 01:44:43 -0800

    typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
+
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
 
-export async function getServerSideProps() {
+export const getServerSideProps: GetServerSideProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
   };
-}
+};
 
 export default function Home({ allPostsData }) {
   return (
