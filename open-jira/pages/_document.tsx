fd59480e91ff0824f521cba8edd3b30b7b2c0b54
tokatu4561 <ryobsk4561@gmail.com>
2022-03-30 08:12:35 -0700

    add document

---
+++ open-jira/pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link rel="shortcut icon" href="/favicon.ico"></link>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
