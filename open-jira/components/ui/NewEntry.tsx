368cd9aa54b8fff37853d8680f00f486f00d7a73
tokatu4561 <ryobsk4561@gmail.com>
2022-04-02 07:33:23 -0700

    add toggle new entry form

--- open-jira/components/ui/NewEntry.tsx
+++ open-jira/components/ui/NewEntry.tsx
@@ -1,29 +1,57 @@
 import { Box, Button, TextField } from "@mui/material";
 import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
-import React, { FC } from "react";
+import AddIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import React, { ChangeEvent, FC, useState } from "react";
 
 export const NewEntry: FC = () => {
+  const [isAddingEntry, setIsAddingEntry] = useState(true);
+  const [inputValue, setInputValue] = useState("");
+  const [touched, setTouched] = useState(false);
+
+  const onTextFieldChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
-    <>
-      <TextField
-        fullWidth
-        sx={{ marginTop: 2, marginBottom: 1 }}
-        placeholder="例：水を買う"
-        autoFocus
-        multiline
-        label="Nueva entrada"
-        helperText="入力してください"
-      />
-      <Box display="flex" justifyContent="space-between">
-        <Button variant="text">キャンセル</Button>
+    <Box sx={{ marginBottom: 2, paddingX: 2 }}>
+      {isAddingEntry ? (
+        <>
+          <TextField
+            fullWidth
+            sx={{ marginTop: 2, marginBottom: 1 }}
+            placeholder="例：水を買う"
+            autoFocus
+            multiline
+            label="Nueva entrada"
+            helperText={inputValue.length <= 0 && touched && "入力してください"}
+            error={inputValue.length <= 0 && touched}
+            value={inputValue}
+            onChange={onTextFieldChanged}
+            onBlur={() => setTouched(true)}
+          />
+          <Box display="flex" justifyContent="space-between">
+            <Button variant="text" onClick={() => setIsAddingEntry(false)}>
+              キャンセル
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              endIcon={<SaveOutlinedIcon />}
+            >
+              追加
+            </Button>
+          </Box>
+        </>
+      ) : (
         <Button
+          startIcon={<AddIcon />}
+          fullWidth
           variant="outlined"
-          color="secondary"
-          endIcon={<SaveOutlinedIcon />}
+          onClick={() => setIsAddingEntry(true)}
         >
-          追加
+          Agregar Tarea
         </Button>
-      </Box>
-    </>
+      )}
+    </Box>
   );
 };
