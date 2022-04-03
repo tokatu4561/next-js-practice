9745678ba0e9ad5b21324abb208a9bb61686035d
tokatu4561 <ryobsk4561@gmail.com>
2022-04-03 15:36:31 -0700

    add db connect by mongoose

---
+++ open-jira/database/db.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+/**
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+ */
+const mongooConnection = {
+  isConnected: 0,
+};
+
+export const connect = async () => {
+  if (mongooConnection.isConnected) {
+    console.log("接続しました");
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    mongooConnection.isConnected = mongoose.connections[0].readyState;
+
+    if (mongooConnection.isConnected === 1) {
+      console.log("以前の接続を使用する");
+      return;
+    }
+
+    await mongoose.disconnect();
+  }
+
+  await mongoose.connect(process.env.MONGO_URL || "");
+  mongooConnection.isConnected = 1;
+  console.log("Conectado a MongoDB:", process.env.MONGO_URL);
+};
+
+export const disconnect = async () => {
+  if (process.env.NODE_ENV === "development") return;
+
+  if (mongooConnection.isConnected === 0) return;
+
+  await mongoose.disconnect();
+  console.log("切断しました");
+};
