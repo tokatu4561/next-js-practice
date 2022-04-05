3bb1bb5af1dca4b0f4aa90a3b1be18966dacaab8
tokatu4561 <ryobsk4561@gmail.com>
2022-04-04 20:22:55 -0700

    add new Entry api

--- open-jira/pages/api/entries/index.ts
+++ open-jira/pages/api/entries/index.ts
@@ -13,6 +13,9 @@ export default function handler(
     case "GET":
       return getEntries(res);
 
+    case "POST":
+      return postEntry(req, res);
+
     default:
       return res.status(400).json({ message: "GETメソッドのみ許可しています" });
   }
@@ -24,4 +27,25 @@ const getEntries = async (res: NextApiResponse<Data>) => {
   await db.disconnect();
 
   return res.status(200).json(entries);
+};
+
+const postEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { description } = req.body;
+  const newEntry = new EntryModel({
+    description,
+    createdAt: Date.now(),
+  });
+
+  try {
+    await db.connect();
+    await newEntry.save();
+    await db.disconnect();
+
+    return res.status(201).json(newEntry);
+  } catch (error) {
+    await db.disconnect();
+    console.log(error);
+
+    return res.status(500).json({ message: error });
+  }
 };
