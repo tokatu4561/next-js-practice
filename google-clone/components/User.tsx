1e2a30e0e579e040b5eb491e868cfe03502dd126
tokatu4561 <ryobsk4561@gmail.com>
2022-04-13 19:35:46 -0700

    add feat google sign in

--- google-clone/components/User.tsx
+++ google-clone/components/User.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { signIn, useSession, signOut } from "next-auth/react";
 
-export default function User() {
-  return <div>User</div>;
-}
+export const User = () => {
+  const { data: session } = useSession();
+
+  if (session && session.user) {
+    return (
+      <>
+        <img
+          onClick={function () {
+            signOut();
+          }}
+          src={session.user.image}
+          alt="user-image"
+          className={`h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1 `}
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <button
+        className={`bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md`}
+        onClick={function () {
+          signIn();
+        }}
+      >
+        Sign in
+      </button>
+    </>
+  );
+};
---
+++ google-clone/pages/auth/signin.tsx
@@ -0,0 +1,51 @@
+import type { NextPage } from "next";
+import Header from "../../components/Header";
+import {
+  ClientSafeProvider,
+  getProviders,
+  LiteralUnion,
+  signIn,
+} from "next-auth/react";
+import { BuiltInProviderType } from "next-auth/providers";
+
+interface Props {
+  providers: Promise<Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null>;
+}
+
+export default function signin({ providers }: Props) {
+  return (
+    <>
+      <Header />
+      <div className="mt-40">
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name} className="flex flex-col items-center">
+            <img
+              className="w-52 object-cover"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+              alt="google-logo"
+            />
+            <p className="text-sm italic my-10 text-center">
+              This website is created for learning purposes
+            </p>
+            <button
+              className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
