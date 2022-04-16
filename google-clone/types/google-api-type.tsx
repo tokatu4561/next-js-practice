c239a5ea2791bd5587a18116274bd8de104302f3
tokatu4561 <ryobsk4561@gmail.com>
2022-04-15 18:56:59 -0700

    add type goole api resulty

---
+++ google-clone/types/google-api-type.tsx
@@ -0,0 +1,248 @@
+export type GoogleApiResults = {
+  kind: string;
+  url: {
+    type: string;
+    template: string;
+  };
+  queries: {
+    request: [
+      {
+        title: string;
+        totalResults: string;
+        searchTerms: string;
+        count: number;
+        startIndex: number;
+        inputEncoding: string;
+        outputEncoding: string;
+        safe: string;
+        cx: string;
+        searchType: string;
+      }
+    ];
+    nextPage: [
+      {
+        title: string;
+        totalResults: string;
+        searchTerms: string;
+        count: number;
+        startIndex: number;
+        inputEncoding: string;
+        outputEncoding: string;
+        safe: string;
+        cx: string;
+        searchType: string;
+      }
+    ];
+  };
+  context: {
+    title: string;
+  };
+  searchInformation: {
+    searchTime: number;
+    formattedSearchTime: string;
+    totalResults: string;
+    formattedTotalResults: string;
+  };
+  items: [
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      mime: string;
+      fileFormat: string;
+      image: {
+        contextLink: string;
+        height: number;
+        width: number;
+        byteSize: number;
+        thumbnailLink: string;
+        thumbnailHeight: number;
+        thumbnailWidth: number;
+      };
+    },
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      mime: string;
+      fileFormat: string;
+      image: {
+        contextLink: string;
+        height: number;
+        width: number;
+        byteSize: number;
+        thumbnailLink: string;
+        thumbnailHeight: number;
+        thumbnailWidth: number;
+      };
+    },
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      mime: string;
+      fileFormat: string;
+      image: {
+        contextLink: string;
+        height: number;
+        width: number;
+        byteSize: number;
+        thumbnailLink: string;
+        thumbnailHeight: number;
+        thumbnailWidth: number;
+      };
+    },
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      mime: string;
+      fileFormat: string;
+      image: {
+        contextLink: string;
+        height: number;
+        width: number;
+        byteSize: number;
+        thumbnailLink: string;
+        thumbnailHeight: number;
+        thumbnailWidth: number;
+      };
+    },
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      mime: string;
+      fileFormat: string;
+      image: {
+        contextLink: string;
+        height: number;
+        width: number;
+        byteSize: number;
+        thumbnailLink: string;
+        thumbnailHeight: number;
+        thumbnailWidth: number;
+      };
+    },
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      mime: string;
+      fileFormat: string;
+      image: {
+        contextLink: string;
+        height: number;
+        width: number;
+        byteSize: number;
+        thumbnailLink: string;
+        thumbnailHeight: number;
+        thumbnailWidth: number;
+      };
+    },
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      mime: string;
+      fileFormat: string;
+      image: {
+        contextLink: string;
+        height: number;
+        width: number;
+        byteSize: number;
+        thumbnailLink: string;
+        thumbnailHeight: number;
+        thumbnailWidth: number;
+      };
+    },
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      mime: string;
+      fileFormat: string;
+      image: {
+        contextLink: string;
+        height: number;
+        width: number;
+        byteSize: number;
+        thumbnailLink: string;
+        thumbnailHeight: number;
+        thumbnailWidth: number;
+      };
+    },
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      mime: string;
+      fileFormat: string;
+      image: {
+        contextLink: string;
+        height: number;
+        width: number;
+        byteSize: number;
+        thumbnailLink: string;
+        thumbnailHeight: number;
+        thumbnailWidth: number;
+      };
+    },
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      mime: string;
+      fileFormat: string;
+      image: {
+        contextLink: string;
+        height: number;
+        width: number;
+        byteSize: number;
+        thumbnailLink: string;
+        thumbnailHeight: number;
+        thumbnailWidth: number;
+      };
+    }
+  ];
+};
