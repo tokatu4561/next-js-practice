7dc9609000f9dead8d7b355c2eaa317353f440ff
tokatu4561 <ryobsk4561@gmail.com>
2022-04-15 20:21:34 -0700

    add show search result items

--- google-clone/components/SearchResults.tsx
+++ google-clone/components/SearchResults.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import Parser from "html-react-parser";
 import { GoogleApiResults } from "../types/google-api-type";
 
 interface Props {
@@ -8,10 +9,26 @@ interface Props {
 export const SearchResults: FC<Props> = ({ results }) => {
   return (
     <div className="w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
-      <p className="text-gray-600 text-sm">
+      <p className="text-gray-600 text-sm mb-4">
         About {results.searchInformation.formattedTotalResults} results
         {results.searchInformation.formattedSearchTime}
       </p>
+      {results.items.map((result) => (
+        <div key={result.link} className="max-w-xl mb-8">
+          <div className="group">
+            <a className="text-sm">{result.link}</a>
+            <a
+              className="group-hover:underline decoration-blue-800"
+              href={result.link}
+            >
+              <h2 className="text-xl font-medium text-blue-800">
+                {result.title}
+              </h2>
+            </a>
+          </div>
+          <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
+        </div>
+      ))}
     </div>
   );
 };
