8072ed1125d627565bbaea66410efea7251d54b3
tokatu4561 <ryobsk4561@gmail.com>
2022-04-21 16:50:41 -0700

    add menu and profile image of the header components

--- instagram-clone/components/Header.tsx
+++ instagram-clone/components/Header.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import React from "react";
-import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { SearchIcon, PlusCircleIcon, HomeIcon } from "@heroicons/react/outline";
 
 export const Header = () => {
   return (
-    <div className="flex justify-between items-center max-w-6xl">
+    <div className="flex justify-between items-center max-w-6xl mx-4 md:mx-auto">
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-block">
         {/* 左側 */}
         <Image
@@ -23,7 +23,6 @@ export const Header = () => {
       </div>
 
       {/* 真中 */}
-
       <div className="relative">
         <SearchIcon className="absolute top-3 left-2 h-4" />
         <input
@@ -34,8 +33,13 @@ export const Header = () => {
           className="pl-8 bg-gray-50 border-gray-500 text-gray-500 focus:border-black focus:ring-black rounded-md"
         />
       </div>
+
       {/* 右側 */}
-      <h1>right side</h1>
+      <div className="flex items-center space-x-4">
+        <HomeIcon className="hidden md:inline-block h-8 cursor-pointer transition-transform hover:scale-125 duration-200" />
+        <PlusCircleIcon className="h-8 cursor-pointer transition-transform hover:scale-125 duration-200" />
+        <img src="" alt="" className="h-10 rounded-full cursor-pointer" />
+      </div>
     </div>
   );
 };
