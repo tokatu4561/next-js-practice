6203a7ab7372880bb662137aa67e450ee5a5f9ac
tokatu4561 <ryobsk4561@gmail.com>
2022-04-21 08:10:50 -0700

    add search form of the header component

--- instagram-clone/components/Header.tsx
+++ instagram-clone/components/Header.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import React from "react";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
 
 export const Header = () => {
   return (
     <div className="flex justify-between items-center max-w-6xl">
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-block">
+        {/* 左側 */}
         <Image
           src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
           layout="fill"
@@ -20,6 +22,19 @@ export const Header = () => {
         />
       </div>
 
+      {/* 真中 */}
+
+      <div className="relative">
+        <SearchIcon className="absolute top-3 left-2 h-4" />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="検索"
+          className="pl-8 bg-gray-50 border-gray-500 text-gray-500 focus:border-black focus:ring-black rounded-md"
+        />
+      </div>
+      {/* 右側 */}
       <h1>right side</h1>
     </div>
   );
