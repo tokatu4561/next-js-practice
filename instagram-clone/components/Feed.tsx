516c7100de7fd4017f00b371eff444a355680080
tokatu4561 <ryobsk4561@gmail.com>
2022-04-23 00:04:37 -0700

    style feed section responsive

--- instagram-clone/components/Feed.tsx
+++ instagram-clone/components/Feed.tsx
@@ -4,13 +4,15 @@ import Stories from "./Stories";
 
 export const Feed = () => {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl">
+      <section className="md:col-span-2">
         {/* ストーリー */}
         <Stories />
         {/* 投稿 */}
         <Posts />
       </section>
+
+      <section className="hidden md:inline-block md:col-span-1"></section>
     </main>
   );
 };
