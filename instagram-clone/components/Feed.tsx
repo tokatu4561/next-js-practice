695a1c81aaac3f697cc1fb7192bdb945cabcd26b
tokatu4561 <ryobsk4561@gmail.com>
2022-04-22 20:03:22 -0700

    fake data using minifaker

---
+++ instagram-clone/components/Feed.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Feed = () => {
+  return (
+    <main>
+      <section>
+        {/* ストーリー */}
+
+        {/* 投稿 */}
+      </section>
+    </main>
+  );
+};
