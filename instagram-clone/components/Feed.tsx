2bc896b4ef8bda2acdd8f5d88ab9c87595ecb27b
tokatu4561 <ryobsk4561@gmail.com>
2022-04-25 22:10:17 -0700

    modify miniProfile and story component to include the session

--- instagram-clone/components/Feed.tsx
+++ instagram-clone/components/Feed.tsx
@@ -1,3 +1,4 @@
+import { useSession } from "next-auth/react";
 import React from "react";
 import { MiniProfile } from "./MiniProfile";
 import { Posts } from "./Posts";
@@ -5,8 +6,16 @@ import Stories from "./Stories";
 import { Suggestions } from "./Suggestions";
 
 export const Feed = () => {
+  const { data: session } = useSession();
+
   return (
-    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl">
+    <main
+      className={`grid mx-auto ${
+        session
+          ? "grid-cols-1 md:grid-cols-3 md:max-w-6xl"
+          : "md:grid-cols-2 md:max-w-3xl"
+      } `}
+    >
       <section className="md:col-span-2">
         {/* ストーリー */}
         <Stories />
--- instagram-clone/components/MiniProfile.tsx
+++ instagram-clone/components/MiniProfile.tsx
@@ -1,12 +1,29 @@
+import { signOut, useSession } from "next-auth/react";
+
 export const MiniProfile = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
-      <img className="h-16 rounded-full border p-[2px]" alt="user-image" />
+      <img
+        src={session && session.user?.image}
+        className="h-16 rounded-full border p-[2px]"
+        alt="user-image"
+      />
       <div className="flex-1 ml-4">
-        <h2 className="font-bold">ああああ</h2>
-        <h3 className="text-sm text-gray-400">Welcome to instagram</h3>
+        <h2 className="font-bold">{session?.user?.name}</h2>
+        <h3 className="text-sm text-gray-400">
+          Welcome to instagram clone app
+        </h3>
       </div>
-      <button className="font-semibold text-blue-400 text-sm">Sign out</button>
+      <button
+        onClick={function () {
+          signOut();
+        }}
+        className="font-semibold text-blue-400 text-sm"
+      >
+        Sign out
+      </button>
     </div>
   );
 };
